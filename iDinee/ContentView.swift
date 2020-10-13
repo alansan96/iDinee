c6eee61f3e1380de66d6f3f88b239a7ffb3fff88
Alan <alansan96@hotmail.com>
2020-10-13 19:07:59 +0700

    Initial Commit

---
+++ iDinee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDinee
+//
+//  Created by Alan Santoso on 13/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDinee/iDineeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineeApp.swift
+//  iDinee
+//
+//  Created by Alan Santoso on 13/10/20.
+//
+
+import SwiftUI
+
+@main
+struct iDineeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
